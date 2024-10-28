a34f290c61296fee2db6cc8828efeaa60d9e4b34
abdurashidovvv <abdurashidovsaydullo35@example.com>
2024-10-28 09:39:36 +0500

    go router

--- lib/presentation/routes/go_router.dart
+++ lib/presentation/routes/go_router.dart
@@ -1,10 +1,6 @@
 import 'package:chatting/presentation/screens/home_screen/home_screen.dart';
 import 'package:chatting/presentation/screens/onboarding_screen/onboarding_screen.dart';
-import 'package:chatting/presentation/screens/splash_screen/splash_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
 
 GoRouter router(String initialRoute) {
   return GoRouter(
